8069019762df7a7ee677dc77ca771476b734b99a
Maksim Shurupov <geomer198@gmail.com>
2025-01-23 18:24:03 +0300

    [FIX] purchase_currency: Code review fixing

--- purchase_currency/models/purchase_order.py
+++ purchase_currency/models/purchase_order.py
@@ -1,22 +1,17 @@
 # Copyright (C) 2025 Cetmix OÜ
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    @api.model
-    def default_get(self, fields_list):
-        result = super().default_get(fields_list)
-        # Check currency_id field exists
-        if "currency_id" not in fields_list:
-            return result
-        default_purchase_currency = self.env.company.default_purchase_currency_id
-        if default_purchase_currency:
-            result["currency_id"] = default_purchase_currency.id
-        return result
+    currency_id = fields.Many2one(default=lambda self: self._default_currency_id())
+
+    def _default_currency_id(self):
+        company = self.env.company
+        return company.default_purchase_currency_id or company.currency_id
 
     @api.onchange("partner_id", "company_id")
     def onchange_partner_id(self):
